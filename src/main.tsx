c4bfbc50ef0207d907e975b8abfb622a3c35c965
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2026-04-09 20:59:15 +0200

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
-import { StrictMode } from "react"
+import { App } from "./App.tsx"
 import { createRoot } from "react-dom/client"
-
+import { StrictMode } from "react"
+import { ThemeProvider } from "@/store/theme/theme-provider.tsx"
 import "./index.css"
-import App from "./App.tsx"
-import { ThemeProvider } from "@/components/theme-provider.tsx"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
