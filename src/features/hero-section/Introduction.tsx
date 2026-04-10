a84b620310d78a25ff8b057a38834ff9f37e5cea
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2026-04-10 12:51:09 +0200

    Style Introduction part in HeroSection

---
+++ src/features/hero-section/Introduction.tsx
@@ -0,0 +1,20 @@
+export const Introduction = () => {
+  return (
+    <div className="absolute top-[40%] left-4 z-1 -translate-y-[40%] lg:static lg:translate-none">
+      <h2 className="text text-2xl leading-[1.1] font-bold text-white sm:text-5xl lg:text-inherit">
+        Hi
+      </h2>
+      <h2 className="text-2xl leading-[1.1] font-bold text-white sm:text-5xl lg:text-inherit">
+        I'm <span className="text-orange-500">Jerzy</span>
+      </h2>
+      <h2 className="text-2xl leading-[1.1] font-medium text-white sm:text-5xl lg:text-inherit">
+        Frontend developer
+      </h2>
+      <p className="mt-2 text-sm text-white sm:text-base lg:text-inherit">
+        Experience with React.js, Javascript, TypeScript, CSS and SCSS, focused
+        on creating user-friendly interfaces and exploring Next.js, Express.js,
+        and MongoDB.
+      </p>
+    </div>
+  )
+}
