0d1e728dcf6fcc0f0714e7ade9e49685c74d07b2
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2026-04-10 21:41:09 +0200

    Create Footer component

--- src/features/footer/Footer.tsx
+++ src/features/footer/Footer.tsx
@@ -1,3 +1,11 @@
 export const Footer = () => {
-  return <footer>Footer</footer>
+  return (
+    <footer className="bg-black/90 p-5">
+      <div className="mx-auto max-w-300">
+        <p className="text-xs text-white">
+          © {new Date().getFullYear()} All Rights Reserved
+        </p>
+      </div>
+    </footer>
+  )
 }
