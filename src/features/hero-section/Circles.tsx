fef0e30c5aa4bac0992889a9eb3f8c31aa625a7f
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2026-04-10 12:50:24 +0200

    Style cirlces reponsivness

---
+++ src/features/hero-section/Circles.tsx
@@ -0,0 +1,13 @@
+export const Circles = () => {
+  return (
+    <div className="absolute right-0 z-1 aspect-square h-full translate-x-1/2 lg:top-1/2 lg:right-auto lg:-left-56 lg:z-auto lg:h-auto lg:w-auto lg:translate-x-0 lg:-translate-y-1/2">
+      <div className="relative h-full w-full lg:h-150 lg:w-150">
+        <div className="absolute inset-0 rounded-full border-34 border-[#FF851B]/60" />
+
+        <div className="absolute inset-10 rounded-full border-25 border-[#FF851B]/40" />
+
+        <div className="absolute inset-18 rounded-full border-26 border-[#FF851B]/40" />
+      </div>
+    </div>
+  )
+}
