24afe93f471497115ef1146dc7c8fa2ad95ce769
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2026-04-10 17:06:01 +0200

    Add badge component from shadcn

---
+++ src/components/ui/badge/badge.tsx
@@ -0,0 +1,26 @@
+import * as React from "react"
+import { badgeVariants } from "./variants"
+import { cn } from "@/lib/utils"
+import { Slot } from "radix-ui"
+import { type VariantProps } from "class-variance-authority"
+
+function Badge({
+  className,
+  variant = "default",
+  asChild = false,
+  ...props
+}: React.ComponentProps<"span"> &
+  VariantProps<typeof badgeVariants> & { asChild?: boolean }) {
+  const Comp = asChild ? Slot.Root : "span"
+
+  return (
+    <Comp
+      data-slot="badge"
+      data-variant={variant}
+      className={cn(badgeVariants({ variant }), className)}
+      {...props}
+    />
+  )
+}
+
+export { Badge }
