38a97e5ec41b1b80b545a0eb6954e1229f1c30d8
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2026-04-10 12:50:45 +0200

    Style HeroSection to be responsive

--- src/features/hero-section/HeroSection.tsx
+++ src/features/hero-section/HeroSection.tsx
@@ -1,3 +1,25 @@
+import { ActionButtons, Circles, Introduction } from "@/features"
+
 export const HeroSection = () => {
-  return <div>HeroSection</div>
+  return (
+    <section className="overflow-hidden lg:overflow-visible lg:px-4 lg:py-30">
+      <div className="relative mx-auto flex max-w-300 flex-col gap-4 lg:flex-row">
+        <div className="order-1 flex w-[40%] flex-col justify-center lg:order-0">
+          <Introduction />
+          <ActionButtons />
+        </div>
+        <div className="relative flex items-center lg:w-[60%]">
+          <Circles />
+          <div className="relative">
+            <img
+              className="h-full min-h-100 sm:h-auto sm:min-h-auto lg:rounded-tl-4xl lg:rounded-br-4xl"
+              src="./desktop.jpeg"
+              alt="Desktop"
+            />
+            <div className="absolute top-0 right-0 bottom-0 rounded-br-4xl bg-amber-600/45 lg:w-[50%]"></div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
