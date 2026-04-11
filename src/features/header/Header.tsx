dca7666965f56bc4d8ad2afc1fa04eb2757129f8
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2026-04-11 12:50:39 +0200

    Change Header to be fixed when scroll page

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -11,7 +11,7 @@ const menuItems = ["Skills", "Projects", "About", "Contact"]
 
 export const Header = () => {
   return (
-    <header className="px-4 py-4">
+    <header className="fixed top-0 left-0 z-3 w-full bg-background px-4 py-4">
       <div className="mx-auto flex max-w-300 items-center justify-between">
         <h1 className="text-2xl font-bold">
           <span className="text-orange-500">&lt; Port</span>
