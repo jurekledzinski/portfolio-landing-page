02db9984e9436b306060527f0f5cd120abcb0903
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2026-04-12 15:40:57 +0200

    Add animations for SkillCard when user hover on it and add styles for animation when user scroll page first time

--- src/features/skills-section/SkillCard.tsx
+++ src/features/skills-section/SkillCard.tsx
@@ -2,9 +2,18 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components"
 import { Icon } from "@iconify/react"
 import type { SkillCardProps } from "./types"
 
-export const SkillCard = ({ description, icon, title }: SkillCardProps) => {
+export const SkillCard = ({
+  className,
+  description,
+  icon,
+  style,
+  title,
+}: SkillCardProps) => {
   return (
-    <Card className="rounded-xs">
+    <Card
+      className={`-translate-y-2 rounded-xs opacity-0 before:absolute before:bottom-0 before:left-0 before:h-0.5 before:w-0 before:bg-orange-400 before:transition-[width] before:duration-500 hover:bg-black/1 hover:before:w-full hover:before:content-[''] dark:hover:bg-card/85 ${className}`}
+      style={style}
+    >
       <CardHeader>
         <Icon icon={icon} className="text-3xl text-orange-400" />
         <CardTitle>{title}</CardTitle>
