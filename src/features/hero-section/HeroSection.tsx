1f7a2faee38ed9a5e89fdb79b45b21cc8befce8f
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2026-04-12 20:23:35 +0200

    Add onClick prop for ActionButtons HeroSection components

--- src/features/hero-section/HeroSection.tsx
+++ src/features/hero-section/HeroSection.tsx
@@ -1,7 +1,11 @@
 import { ActionButtons, Circles, Introduction, HeroImage } from "@/features"
 import type { HeroSectionProps } from "./types"
 
-export const HeroSection = ({ ref, visibleSections }: HeroSectionProps) => {
+export const HeroSection = ({
+  ref,
+  onClick,
+  visibleSections,
+}: HeroSectionProps) => {
   return (
     <section
       className="overflow-hidden pt-17 lg:overflow-visible lg:px-4 lg:pt-47 lg:pb-30"
@@ -11,7 +15,7 @@ export const HeroSection = ({ ref, visibleSections }: HeroSectionProps) => {
       <div className="relative mx-auto flex max-w-300 flex-col gap-4 lg:flex-row">
         <div className="order-1 flex w-[40%] flex-col justify-center lg:order-0">
           <Introduction />
-          <ActionButtons />
+          <ActionButtons onClick={onClick} />
         </div>
 
         <div className="relative flex items-center lg:w-[60%]">
