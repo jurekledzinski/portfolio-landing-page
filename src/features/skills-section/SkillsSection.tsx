5fda1638c806a266331666aa47b421f8fb1a63a4
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2026-04-12 15:42:01 +0200

    Add animtion when user scroll page to SkillSection and add props

--- src/features/skills-section/SkillsSection.tsx
+++ src/features/skills-section/SkillsSection.tsx
@@ -1,9 +1,11 @@
+import clsx from "clsx"
 import { SkillCard } from "./SkillCard"
 import { skillsData } from "./utils"
+import type { SkillsSectionProps } from "./types"
 
-export const SkillsSection = () => {
+export const SkillsSection = ({ ref, visibleSections }: SkillsSectionProps) => {
   return (
-    <section className="px-4 pt-20 pb-10 lg:pt-30">
+    <section className="px-4 pt-20 pb-10 lg:pt-30" id="skills" ref={ref}>
       <div className="mx-auto max-w-300">
         <h2 className="text-2xl font-bold sm:text-4xl">Skills & Stack</h2>
         <p className="mt-2 text-sm text-secondary sm:text-base">
@@ -11,11 +13,16 @@ export const SkillsSection = () => {
         </p>
 
         <div className="mt-10 grid grid-cols-1 gap-2 text-sm sm:not-first:grid-cols-[repeat(auto-fit,minmax(200px,1fr))]">
-          {skillsData.map((skill) => (
+          {skillsData.map((skill, index) => (
             <SkillCard
               key={skill.title}
+              className={clsx({
+                "translate-y-0 opacity-100 transition-[transform,opacity] duration-500 ease-in":
+                  visibleSections.has("skills"),
+              })}
               description={skill.description}
               icon={skill.icon}
+              style={{ transitionDelay: `${index * 300}ms` }}
               title={skill.title}
             />
           ))}
--- src/features/skills-section/types.ts
+++ src/features/skills-section/types.ts
@@ -1,7 +1,14 @@
 import type { IconifyIcon } from "@iconify/react"
 
 export type SkillCardProps = {
+  className: string
   description: string
   icon: IconifyIcon
   title: string
+  style?: React.CSSProperties
+}
+
+export type SkillsSectionProps = {
+  ref: React.Ref<HTMLElement>
+  visibleSections: Set<string>
 }
