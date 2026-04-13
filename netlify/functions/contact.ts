9fd56ac9f7095b818ff037c3b700bb189e92483b
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2026-04-13 18:29:44 +0200

    Add neltify functions for send contact message

---
+++ netlify/functions/contact.ts
@@ -0,0 +1,69 @@
+import fs from "fs"
+import handlebars from "handlebars"
+import juice from "juice"
+import nodemailer from "nodemailer"
+import path from "path"
+import { ContactEmailSchema } from "@actions"
+import { tryCatch } from "@netlify-helpers"
+import type { Handler, HandlerEvent } from "@netlify/functions"
+
+const contactEmailTemplate = fs.readFileSync(
+  path.join(__dirname, "../../src/templates/contactEmail.hbs"),
+  "utf-8"
+)
+const compiledTemplate = handlebars.compile(contactEmailTemplate)
+
+export const handler: Handler = tryCatch(async (event: HandlerEvent) => {
+  const body = JSON.parse(event.body!)
+  const parsedData = ContactEmailSchema.parse(body)
+
+  const currentYear = new Date().getFullYear()
+  const date = new Date().toLocaleString()
+  const htmlContent = compiledTemplate({
+    name: parsedData.name,
+    email: parsedData.email,
+    message: parsedData.message,
+    currentYear,
+    date,
+  })
+
+  const cssPath = path.join(
+    process.cwd(),
+    "src",
+    "templates",
+    "contactEmail.css"
+  )
+
+  const cssContent = fs.readFileSync(cssPath, "utf-8")
+
+  const htmlWithCss = juice.inlineContent(htmlContent, cssContent)
+
+  const transporter = nodemailer.createTransport({
+    host: process.env.HOST_EMAIL,
+    port: process.env.PORT_PROVIDER_EMAIL,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.PASSWORD_USER,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  } as nodemailer.TransportOptions)
+
+  const mailOptions = {
+    from: `Portfolio <${process.env.EMAIL_USER}>`,
+    to: process.env.EMAIL_SENDTO,
+    subject: "Contact email",
+    html: htmlWithCss,
+  }
+
+  await transporter.sendMail(mailOptions)
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Message sent successfully",
+    }),
+  }
+})
