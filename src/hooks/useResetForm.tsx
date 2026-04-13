320d61e080b56d9a02a6c1798d6500de5ba0d2ad
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2026-04-13 18:36:37 +0200

    Create global hooks folder and add useResetForm hook

---
+++ src/hooks/useResetForm.tsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react"
+
+type UseResetFormProps = {
+  isPending: boolean
+  isSuccess: boolean
+  isSubmitSuccess: boolean
+  onSuccess: () => void
+  isReset?: boolean
+}
+
+export const useResetForm = ({
+  isPending,
+  isSuccess,
+  isSubmitSuccess,
+  onSuccess,
+  isReset = true,
+}: UseResetFormProps) => {
+  useEffect(() => {
+    if (isSubmitSuccess && !isPending && isSuccess) {
+      if (isReset) onSuccess()
+    }
+  }, [isPending, isReset, isSubmitSuccess, isSuccess, onSuccess])
+}
