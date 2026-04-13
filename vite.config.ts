bf1f4bea0cd0883ae4fe30c5927e37e6dbedbd9b
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2026-04-13 18:29:15 +0200

    Update aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import path from "path"
-import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "vite"
 
 // https://vite.dev/config/
@@ -9,6 +9,13 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@actions": path.resolve(__dirname, "src/actions"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@features": path.resolve(__dirname, "src/features"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@lib": path.resolve(__dirname, "src/lib"),
+      "@netlify-helpers": path.resolve(__dirname, "./netlify-helpers"),
+      "@store": path.resolve(__dirname, "src/store"),
     },
   },
 })
