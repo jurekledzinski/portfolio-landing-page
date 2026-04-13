35ba0381065f6a2a17f4326a03dacff0d9d51344
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2026-04-13 16:29:28 +0200

    Update url to image in HeroImage component

--- src/features/hero-section/HeroImage.tsx
+++ src/features/hero-section/HeroImage.tsx
@@ -3,7 +3,7 @@ export const HeroImage = () => {
     <div className="relative">
       <img
         className="h-full min-h-100 sm:h-auto sm:min-h-auto lg:rounded-tl-4xl lg:rounded-br-4xl"
-        src="./desktop.jpeg"
+        src="./images/desktop.jpeg"
         alt="Desktop"
       />
       <div className="absolute top-0 right-0 bottom-0 rounded-br-4xl bg-amber-600/45 lg:w-[50%]"></div>
