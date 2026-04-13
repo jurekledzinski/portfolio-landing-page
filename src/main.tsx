175795a541501a735766da19366fb6772c6b9431
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2026-04-13 18:30:54 +0200

    Add tanstack query and toaster provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,28 @@
 import { App } from "./App.tsx"
 import { createRoot } from "react-dom/client"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { StrictMode } from "react"
-import { ThemeProvider } from "@/store"
+import { ThemeProvider } from "@store"
+import { Toaster } from "react-hot-toast"
 import "./index.css"
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <Toaster
+          position="top-left"
+          toastOptions={{
+            duration: 2000,
+            style: {
+              borderRadius: 2,
+            },
+          }}
+        />
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>
 )
