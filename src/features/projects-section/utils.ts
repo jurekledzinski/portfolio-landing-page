64da5fde1562931ac38e1f8cfab1d5f693caedf3
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2026-04-13 16:31:08 +0200

    Add to projectsData conts data live and github properties

--- src/features/projects-section/utils.ts
+++ src/features/projects-section/utils.ts
@@ -1,6 +1,6 @@
 export const projectsData = [
   {
-    image: "./place-quest.webp",
+    image: "./images/place-quest.webp",
     title: "Place Quest",
     description:
       "A modern, responsive marketplace application built with Next.js TypeScript.",
@@ -17,9 +17,11 @@ export const projectsData = [
       "Zod",
       "Imagekit",
     ],
+    live: import.meta.env.VITE_PLACE_QUEST_LIVE_URL,
+    github: import.meta.env.VITE_PLACE_QUEST_GITHUB_URL,
   },
   {
-    image: "./shoppy.webp",
+    image: "./images/shoppy.webp",
     title: "Shoppy",
     description: "A small e-commerce UI built with Next.js and CSS modules.",
     tags: [
@@ -33,9 +35,11 @@ export const projectsData = [
       "Nodemailer",
       "Handlebars",
     ],
+    live: import.meta.env.VITE_SHOPPY_URL,
+    github: import.meta.env.VITE_SHOPPY_GITHUB_URL,
   },
   {
-    image: "./daily-news.webp",
+    image: "./images/daily-news.webp",
     title: "Daily News",
     description:
       "A responsive MERN web app built with React and the the quardian free API.",
@@ -50,12 +54,327 @@ export const projectsData = [
       "Passport.js",
       "Zustand",
     ],
+    live: import.meta.env.VITE_DAILY_NEWS_URL,
+    github: import.meta.env.VITE_DAILY_NEWS_GITHUB_URL,
   },
   {
-    image: "./storybook.webp",
+    image: "./images/storybook.webp",
     title: "Storybook components",
     description:
       "An interactive Storybook showcasing my reusable UI components",
     tags: ["Storybook", "Typescript"],
+    live: import.meta.env.VITE_STORYBOOK_URL,
+    github: import.meta.env.VITE_STORYBOOK_GITHUB_URL,
   },
 ]
+
+export const screenshotsProject = {
+  place_quest: [
+    {
+      id: 1,
+      src: "images/houses/houses_home_light_1.png",
+    },
+    {
+      id: 2,
+      src: "images/houses/houses_home_dark_1.png",
+    },
+    {
+      id: 3,
+      src: "images/houses/houses_details_light_1.png",
+    },
+    {
+      id: 4,
+      src: "images/houses/houses_details_dark_1.png",
+    },
+    {
+      id: 5,
+      src: "images/houses/houses_login_light_1.png",
+    },
+    {
+      id: 6,
+      src: "images/houses/houses_login_dark_1.png",
+    },
+    {
+      id: 7,
+      src: "images/houses/houses_register_light_1.png",
+    },
+    {
+      id: 8,
+      src: "images/houses/houses_register_dark_1.png",
+    },
+    {
+      id: 9,
+      src: "images/houses/houses_dashboard_light_1.png",
+    },
+    {
+      id: 10,
+      src: "images/houses/houses_dashboard_light_2.png",
+    },
+    {
+      id: 11,
+      src: "images/houses/houses_dashboard_dark_1.png",
+    },
+    {
+      id: 12,
+      src: "images/houses/houses_dashboard_dark_2.png",
+    },
+    {
+      id: 13,
+      src: "images/houses/houses_add_advert_light_1.png",
+    },
+    {
+      id: 14,
+      src: "images/houses/houses_add_advert_dark_1.png",
+    },
+    {
+      id: 15,
+      src: "images/houses/houses_edit_advert_light_1.png",
+    },
+    {
+      id: 16,
+      src: "images/houses/houses_edit_advert_dark_1.png",
+    },
+    {
+      id: 17,
+      src: "images/houses/houses_table_light_1.png",
+    },
+    {
+      id: 18,
+      src: "images/houses/houses_table_light_2.png",
+    },
+    {
+      id: 19,
+      src: "images/houses/houses_table_dark_1.png",
+    },
+    {
+      id: 20,
+      src: "images/houses/houses_table_dark_2.png",
+    },
+    {
+      id: 21,
+      src: "images/houses/houses_profile_light_1.png",
+    },
+    {
+      id: 22,
+      src: "images/houses/houses_profile_dark_1.png",
+    },
+    {
+      id: 23,
+      src: "images/houses/houses_messages_light_1.png",
+    },
+    {
+      id: 24,
+      src: "images/houses/houses_messages_light_2.png",
+    },
+    {
+      id: 25,
+      src: "images/houses/houses_messages_light_3.png",
+    },
+    {
+      id: 26,
+      src: "images/houses/houses_messages_dark_1.png",
+    },
+    {
+      id: 27,
+      src: "images/houses/houses_messages_dark_2.png",
+    },
+    {
+      id: 28,
+      src: "images/houses/houses_messages_dark_3.png",
+    },
+  ],
+  shoppy: [
+    {
+      id: 1,
+      src: "images/shoppy/shoppy_home_light_1.png",
+    },
+    {
+      id: 2,
+      src: "images/shoppy/shoppy_home_dark_1.png",
+    },
+    {
+      id: 3,
+      src: "images/shoppy/shoppy_list_light_1.png",
+    },
+    {
+      id: 4,
+      src: "images/shoppy/shoppy_list_dark_1.png",
+    },
+    {
+      id: 5,
+      src: "images/shoppy/shoppy_details_light_1.png",
+    },
+    {
+      id: 6,
+      src: "images/shoppy/shoppy_details_dark_1.png",
+    },
+    {
+      id: 7,
+      src: "images/shoppy/shoppy_sign_in_light_1.png",
+    },
+    {
+      id: 8,
+      src: "images/shoppy/shoppy_sign_in_dark_1.png",
+    },
+    {
+      id: 9,
+      src: "images/shoppy/shoppy_sign_up_light_1.png",
+    },
+    {
+      id: 10,
+      src: "images/shoppy/shoppy_sign_up_dark_1.png",
+    },
+    {
+      id: 11,
+      src: "images/shoppy/shoppy_contact_light_1.png",
+    },
+    {
+      id: 12,
+      src: "images/shoppy/shoppy_contact_dark_1.png",
+    },
+    {
+      id: 13,
+      src: "images/shoppy/shoppy_menu_light_1.png",
+    },
+    {
+      id: 14,
+      src: "images/shoppy/shoppy_menu_light_2.png",
+    },
+    {
+      id: 15,
+      src: "images/shoppy/shoppy_menu_dark_1.png",
+    },
+    {
+      id: 16,
+      src: "images/shoppy/shoppy_menu_dark_2.png",
+    },
+    {
+      id: 17,
+      src: "images/shoppy/shoppy_recover_light_1.png",
+    },
+    {
+      id: 18,
+      src: "images/shoppy/shoppy_recover_dark_1.png",
+    },
+    {
+      id: 19,
+      src: "images/shoppy/shoppy_empty_card_light_1.png",
+    },
+    {
+      id: 20,
+      src: "images/shoppy/shoppy_empty_card_dark_1.png",
+    },
+    {
+      id: 21,
+      src: "images/shoppy/shoppy_full_card_light_1.png",
+    },
+    {
+      id: 22,
+      src: "images/shoppy/shoppy_full_card_dark_1.png",
+    },
+    {
+      id: 23,
+      src: "images/shoppy/shoppy_list_light_1.png",
+    },
+    {
+      id: 24,
+      src: "images/shoppy/shoppy_list_dark_1.png",
+    },
+    {
+      id: 25,
+      src: "images/shoppy/shoppy_orders_light_1.png",
+    },
+    {
+      id: 26,
+      src: "images/shoppy/shoppy_orders_dark_1.png",
+    },
+    {
+      id: 27,
+      src: "images/shoppy/shoppy_profile_light_1.png",
+    },
+    {
+      id: 28,
+      src: "images/shoppy/shoppy_profile_dark_1.png",
+    },
+    {
+      id: 29,
+      src: "images/shoppy/shoppy_shipping_light_1.png",
+    },
+    {
+      id: 30,
+      src: "images/shoppy/shoppy_shipping_dark_1.png",
+    },
+    {
+      id: 31,
+      src: "images/shoppy/shoppy_place_order_light_1.png",
+    },
+    {
+      id: 32,
+      src: "images/shoppy/shoppy_place_order_dark_1.png",
+    },
+    {
+      id: 33,
+      src: "images/shoppy/shoppy_payment_stripe.png",
+    },
+  ],
+  daily_news: [
+    {
+      id: 1,
+      src: "images/news/news-dashboard-1.webp",
+    },
+    {
+      id: 2,
+      src: "images/news/news-dashboard-2.webp",
+    },
+    {
+      id: 3,
+      src: "images/news/news-login.webp",
+    },
+    {
+      id: 4,
+      src: "images/news/news-register.webp",
+    },
+    {
+      id: 5,
+      src: "images/news/news-grid-articles.webp",
+    },
+    {
+      id: 6,
+      src: "images/news/news-details-1.webp",
+    },
+    {
+      id: 7,
+      src: "images/news/news-details-2.webp",
+    },
+    {
+      id: 8,
+      src: "images/news/news-profile-1.webp",
+    },
+    {
+      id: 9,
+      src: "images/news/news-profile-2.webp",
+    },
+    {
+      id: 10,
+      src: "images/news/news-profile-3.webp",
+    },
+  ],
+  storybook_components: [
+    {
+      id: 1,
+      src: "images/storybook/colors_variables.png",
+    },
+    {
+      id: 2,
+      src: "images/storybook/buttons_doc.png",
+    },
+    {
+      id: 3,
+      src: "images/storybook/input.png",
+    },
+    {
+      id: 4,
+      src: "images/storybook/colors_doc.png",
+    },
+  ],
+}
