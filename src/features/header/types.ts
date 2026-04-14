7dc3fe17ac5334c54fc2139b6eb6dfb98c85060f
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2026-04-14 13:40:16 +0200

    Update HeaderProps

--- src/features/header/types.ts
+++ src/features/header/types.ts
@@ -1,3 +1,14 @@
+import type React from "react"
+
 export type HeaderProps = {
+  children?: React.ReactNode
+}
+
+type MenuBaseProps = {
+  activeItem: string
   onClick: React.MouseEventHandler<HTMLButtonElement | HTMLDivElement>
 }
+
+export type DesktopMenuProps = MenuBaseProps
+export type MobileMenuProps = MenuBaseProps
+export type LogoProps = MenuBaseProps
