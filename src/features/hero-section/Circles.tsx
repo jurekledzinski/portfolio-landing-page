3ae55a889cf8e8bf85a7ec488c1113fd98376af2
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2026-04-14 13:40:45 +0200

    Update animations in Circles component in HeroSection

--- src/features/hero-section/Circles.tsx
+++ src/features/hero-section/Circles.tsx
@@ -1,33 +1,12 @@
-import clsx from "clsx"
-import type { CirclesProps } from "./types"
-
-export const Circles = ({ visibleSections }: CirclesProps) => {
+export const Circles = () => {
   return (
     <div className="absolute right-0 z-1 aspect-square h-full translate-x-1/2 lg:top-1/2 lg:right-auto lg:-left-56 lg:z-auto lg:h-auto lg:w-auto lg:translate-x-0 lg:-translate-y-1/2">
       <div className="relative h-full w-full lg:h-150 lg:w-150">
-        <div
-          className={clsx({
-            "absolute inset-0 rounded-full border-34 border-[#FF851B]/60 opacity-0": true,
-            "translate-y-0 opacity-100 transition-opacity delay-800 duration-500 ease-in":
-              visibleSections.has("hero"),
-          })}
-        />
+        <div className="absolute inset-0 rounded-full border-34 border-[#FF851B]/60 opacity-0 group-[.visible]:translate-y-0 group-[.visible]:opacity-100 group-[.visible]:transition-opacity group-[.visible]:delay-800 group-[.visible]:duration-500 group-[.visible]:ease-in" />
 
-        <div
-          className={clsx({
-            "absolute inset-10 rounded-full border-25 border-[#FF851B]/40 opacity-0": true,
-            "translate-y-0 opacity-100 transition-opacity delay-400 duration-500 ease-in":
-              visibleSections.has("hero"),
-          })}
-        />
+        <div className="absolute inset-10 rounded-full border-25 border-[#FF851B]/40 opacity-0 group-[.visible]:translate-y-0 group-[.visible]:opacity-100 group-[.visible]:transition-opacity group-[.visible]:delay-400 group-[.visible]:duration-500 group-[.visible]:ease-in" />
 
-        <div
-          className={clsx({
-            "absolute inset-18 rounded-full border-26 border-[#FF851B]/40 opacity-0": true,
-            "translate-y-0 opacity-100 transition-opacity delay-0 duration-500 ease-in":
-              visibleSections.has("hero"),
-          })}
-        />
+        <div className="absolute inset-18 rounded-full border-26 border-[#FF851B]/40 opacity-0 group-[.visible]:translate-y-0 group-[.visible]:opacity-100 group-[.visible]:transition-opacity group-[.visible]:delay-0 group-[.visible]:duration-500 group-[.visible]:ease-in" />
       </div>
     </div>
   )
