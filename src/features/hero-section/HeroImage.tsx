06a8d7230ad2b16c46cb3081e72b12ee06efe8fb
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2026-04-16 16:08:53 +0200

    Add layers with cutouts under the hero photo on desktop

--- src/features/hero-section/HeroImage.tsx
+++ src/features/hero-section/HeroImage.tsx
@@ -6,7 +6,22 @@ export const HeroImage = () => {
         src="./images/desktop.jpeg"
         alt="Desktop"
       />
-      <div className="absolute top-0 right-0 bottom-0 rounded-br-4xl bg-amber-600/45 lg:w-[50%]"></div>
+
+      <div className="absolute top-0 right-0 bottom-0 z-5 overflow-hidden rounded-br-4xl lg:w-[58%]">
+        <div className="absolute inset-0 bg-amber-600/30 mask-[radial-gradient(circle_300px_at_-224px_50%,transparent_99%,black_100%)] [-webkit-mask-image:radial-gradient(circle_300px_at_-224px_50%,transparent_99%,black_100%)]"></div>
+      </div>
+
+      <div className="absolute top-0 right-0 bottom-0 z-4 overflow-hidden rounded-br-4xl lg:w-[64.5%]">
+        <div className="absolute inset-0 bg-amber-600/25 mask-[radial-gradient(circle_266px_at_-175px_50%,transparent_99%,black_100%)] [-webkit-mask-image:radial-gradient(circle_283px_at_-198px_50%,transparent_99%,black_100%)]"></div>
+      </div>
+
+      <div className="absolute top-0 right-0 bottom-0 z-3 overflow-hidden rounded-br-4xl lg:w-[72%]">
+        <div className="absolute inset-0 bg-amber-600/20 mask-[radial-gradient(circle_247.5px_at_-143px_50%,transparent_99%,black_100%)] [-webkit-mask-image:radial-gradient(circle_247.5px_at_-143px_50%,transparent_99%,black_100%)]"></div>
+      </div>
+
+      <div className="absolute top-0 right-0 bottom-0 z-2 overflow-hidden rounded-br-4xl lg:w-[84%]">
+        <div className="absolute inset-0 bg-amber-600/15 mask-[radial-gradient(circle_206px_at_-49px_50%,transparent_99%,black_100%)] [-webkit-mask-image:radial-gradient(circle_206px_at_-49px_50%,transparent_99%,black_100%)]"></div>
+      </div>
     </div>
   )
 }
